b7746c11090c5136e067142e2bf25d06fc6d0478
Niels Hartmann <nwhartmann@gmail.com>
2026-04-25 09:55:19 +0200

    Updated depencies for harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,6 +1,7 @@
 return {
   "theprimeagen/harpoon",
   branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local harpoon = require("harpoon").setup({})
   end,
